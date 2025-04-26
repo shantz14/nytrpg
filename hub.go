b77322f5c8340b150a11823e663d7ee01babd370
shantz14 <isaac@steveandkureen.com>
2025-04-26 02:14:52 -0700

    Moved handlePlayer to Player

--- hub.go
+++ hub.go
@@ -1,20 +1,7 @@
 package main
 
 import (
-	"fmt"
-	"time"
-
-	"github.com/gorilla/websocket"
-	"github.com/vmihailenco/msgpack/v5"
 )
-
-type PlayerData struct {
-	ID int `msgpack:"id"`
-	Pos Vector2D `msgpack:"pos"`
-	
-	// True when this is the data of the player being sent to
-	Me bool `msgpack:"me"`
-}
 
 type GameState struct {
 	Players map[int]*PlayerData `msgpack:"players"`
@@ -51,45 +38,4 @@ func (h *Hub) run() {
 	}
 }
 
-func (h *Hub) handlePlayer(p *Player) {
-	updateInterval := time.Second / 30
 
-	for range time.Tick(updateInterval) {
-		// Read
-		_, inBuff, err := p.conn.ReadMessage() // No use for msg type yet
-		if err != nil {
-			fmt.Println("Error reading msg from player: ", err)
-		} else {
-			var inData PlayerData
-			if err := msgpack.Unmarshal(inBuff, &inData); err != nil {
-				fmt.Println("Error unpacking data: ", err)
-			}
-
-			// Assign their id
-			inData.ID = p.id
-
-			h.in <- inData
-		}
-
-		// Write
-		var outData GameState
-		outData = *h.state
-
-		// Set players own data ME flag
-		// TODO: Consolidate these 2 id values
-		outData.Players[p.id].Me = true
-		// SOMEWHERE all players are being set to Me
-		// TODO: Find where this is happening and remove this loop
-		for k := range outData.Players {
-			if k != p.id {
-				outData.Players[k].Me = false
-			}
-		}
-
-		outBuff, err := msgpack.Marshal(outData)
-		if err := p.conn.WriteMessage(websocket.BinaryMessage, outBuff); err != nil {
-			fmt.Println("Error writing msg to player: ", err)
-		}
-	}
-
-}
--- player.go
+++ player.go
@@ -2,9 +2,11 @@ package main
 
 import (
 	"fmt"
+	"time"
 	"net/http"
 
 	"github.com/gorilla/websocket"
+	"github.com/vmihailenco/msgpack/v5"
 )
 
 var upgrader = websocket.Upgrader{
@@ -17,6 +19,14 @@ type Player struct {
 
 	conn *websocket.Conn
 
+}
+
+type PlayerData struct {
+	ID int `msgpack:"id"`
+	Pos Vector2D `msgpack:"pos"`
+	
+	// True when this is the data of the player being sent to
+	Me bool `msgpack:"me"`
 }
 
 func handleWS(h *Hub, w http.ResponseWriter, r *http.Request) {
@@ -36,7 +46,50 @@ func handleWS(h *Hub, w http.ResponseWriter, r *http.Request) {
 	// Add pos to gamestate
 	h.state.Players[newPlayer.id] = &PlayerData{ID: newPlayer.id, Pos: Vector2D{X: 0, Y: 0}}
 
-	go h.handlePlayer(&newPlayer)
+	go newPlayer.handlePlayer(h)
+}
+
+func (p *Player) handlePlayer(h *Hub) {
+	updateInterval := time.Second / 30
+
+	for range time.Tick(updateInterval) {
+		// Read
+		_, inBuff, err := p.conn.ReadMessage() // No use for msg type yet
+		if err != nil {
+			fmt.Println("Error reading msg from player: ", err)
+		} else {
+			var inData PlayerData
+			if err := msgpack.Unmarshal(inBuff, &inData); err != nil {
+				fmt.Println("Error unpacking data: ", err)
+			}
+
+			// Assign their id
+			inData.ID = p.id
+
+			h.in <- inData
+		}
+
+		// Write
+		var outData GameState
+		outData = *h.state
+
+		// Set players own data ME flag
+		// TODO: Consolidate these 2 id values
+		outData.Players[p.id].Me = true
+		// SOMEWHERE all players are being set to Me
+		// TODO: Find where this is happening and remove this loop
+		for k := range outData.Players {
+			if k != p.id {
+				outData.Players[k].Me = false
+			}
+		}
+
+		outBuff, err := msgpack.Marshal(outData)
+		if err := p.conn.WriteMessage(websocket.BinaryMessage, outBuff); err != nil {
+			fmt.Println("Error writing msg to player: ", err)
+		}
+	}
+
 }
 
 
